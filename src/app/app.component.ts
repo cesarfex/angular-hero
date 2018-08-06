e7c1cb5b79974f71dc42c1f675be1c44f3fda511
Augusto <cesarfex@gmail.com>
2018-08-06 19:51:30 -0300

    uso das diretivas ngFor,ngIf e evento click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,81 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Spider-Man' },
+  { id: 12, name: 'Captain America' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Iron Man' },
+  { id: 16, name: 'Luke Cage' },
+  { id: 17, name: 'Doctor Strange' },
+  { id: 18, name: 'Daredevil' },
+  { id: 19, name: 'Ant-Man' },
+  { id: 20, name: 'Wolverine' }
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Hulk'
-  };
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
